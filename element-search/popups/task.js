714b1e8dbb9c8fee6aadc29b7c9278e16c454bc6
SergejBeschastnov <svb19.09@yandex.ru>
2021-08-31 19:41:57 +0300

    1st done

---
+++ element-search/popups/task.js
@@ -0,0 +1,24 @@
+let modals = document.querySelectorAll('.modal');
+
+modals.item(0).classList.add('modal_active');
+
+let firstConfirm = document.querySelector('.show-success'),
+    secondConfirm = document.querySelector('.btn_success');
+
+firstConfirm.onclick = function () {
+    modals.item(1).classList.add('modal_active');
+    modals.item(0).classList.remove('modal_active');
+}
+
+secondConfirm.onclick = function () {
+    modals.item(1).classList.remove('modal_active');
+}
+
+
+let modalClose = document.querySelectorAll('.modal__close_times');
+console.log(modalClose)
+modalClose.forEach(item => item.onclick = function () {
+    let modalParent = item.closest('.modal_active');
+    modalParent.classList.remove('modal_active');
+})
+
